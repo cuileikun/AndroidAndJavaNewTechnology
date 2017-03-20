5ba973c9e811b56f483653143c492fe18bd1ffae
cuileikun <583271702@qq.com>
2017-03-20 15:11:06 +0800

    百度地图 三方分享增加提示语

--- app/src/main/java/com/example/cuileikun/androidandjava/activity/ThirdLiarbryActivity.java
+++ app/src/main/java/com/example/cuileikun/androidandjava/activity/ThirdLiarbryActivity.java
@@ -1,17 +1,19 @@
 package com.example.cuileikun.androidandjava.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.example.cuileikun.androidandjava.R;
-import com.example.cuileikun.androidandjava.activity.thirdliarbry.LocateActivity;
 import com.example.cuileikun.androidandjava.activity.thirdliarbry.SmsVerificationsActivity;
 import com.qk.applibrary.activity.QkActivity;
 import com.qk.applibrary.listener.TopbarImplListener;
+import com.qk.applibrary.util.CommonUtil;
 import com.qk.applibrary.widget.TopbarView;
 
 public class ThirdLiarbryActivity extends QkActivity implements View.OnClickListener {
+    private Context mContext;
     public static ThirdLiarbryActivity mInstance = null;
     private TopbarView topbarView;
     private RelativeLayout sms_verification_rl;//1，短信验证
@@ -20,6 +22,7 @@ public class ThirdLiarbryActivity extends QkActivity implements View.OnClickList
 
     @Override
     public void initViews() {
+        mContext=ThirdLiarbryActivity.this;
         mInstance = ThirdLiarbryActivity.this;
         topbarView = (TopbarView) findViewById(R.id.top_bar_view);
         sms_verification_rl = (RelativeLayout) findViewById(R.id.sms_verification_rl);
@@ -62,8 +65,13 @@ public class ThirdLiarbryActivity extends QkActivity implements View.OnClickList
                 break;
 
             case R.id.three_share_rl:
-                startActivity(new Intent(ThirdLiarbryActivity.this,LocateActivity.class));
+                CommonUtil.sendToast(mContext,"项目已经三方分享demo app，可以独立运行");
                 break;
+
+            case R.id.rl_baidu_locate:
+                CommonUtil.sendToast(mContext,"项目已经导入和百度地图官方demo app，可以独立运行");
+                break;
+
         }
 
     }
