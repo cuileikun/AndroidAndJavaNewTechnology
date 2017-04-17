cfb4f809a3d2fc9fedbdd0c232b233b1e6d65a6f
cuileikun <583271702@qq.com>
2017-04-17 17:01:09 +0800

    android base 02

---
+++ app/src/main/java/com/example/cuileikun/androidandjava/activity/androidbase/LogActivity.java
@@ -0,0 +1,16 @@
+package com.example.cuileikun.androidandjava.activity.androidbase;
+
+import com.example.cuileikun.androidandjava.R;
+import com.qk.applibrary.activity.QkActivity;
+
+/**
+ * 作者：popular cui
+ * 时间：2017/4/17 16:51
+ * 功能:
+ */
+public class LogActivity extends QkActivity{
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_log;
+    }
+}
---
+++ app/src/main/java/com/example/cuileikun/androidandjava/activity/androidbase/UserLoginSdcardActivity.java
@@ -0,0 +1,16 @@
+package com.example.cuileikun.androidandjava.activity.androidbase;
+
+import com.example.cuileikun.androidandjava.R;
+import com.qk.applibrary.activity.QkActivity;
+
+/**
+ * 作者：popular cui
+ * 时间：2017/4/17 16:55
+ * 功能:
+ */
+public class UserLoginSdcardActivity extends QkActivity{
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_user_login_sdcard;
+    }
+}
